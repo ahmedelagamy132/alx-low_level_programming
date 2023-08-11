892cae25d07a5221b87211f1c400e9f9106ceeef
ahmed <ahmedelagamy132@gmail.com>
2023-08-10 18:45:48 -0700

    edit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*
+* main - maaaaaaaaaaaain
+*
+*Return: 1
+*/
+
+int main(void)
+{
+	char sentence[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fputs(sentence, stdout);
+	return (1);
+}
