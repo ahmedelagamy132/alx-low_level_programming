19dfe9f34e9ca20eec2aae7ae78f4ce7ae0d8d8c
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 12:20:47 -0700

    edit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	char car = 'z';
+
+	do {
+	putchar (car);
+	car--;
+	} while (car >= 'a');
+
+	putchar('\n');
+
+return (0);
