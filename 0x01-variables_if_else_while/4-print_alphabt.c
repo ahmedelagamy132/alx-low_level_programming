dccb34bd503c0a1292dc2534c50d3a5d3d33cd46
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 11:54:11 -0700

    edit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	char caracter = 'a';
+
+	do {
+	if (caracter != 'e' && caracter != 'q')
+	{
+	putchar (caracter);
+	}
+	caracter++;
+	} while (caracter <= 'z');
+
+	putchar('\n');
+
+return (0);
+}
