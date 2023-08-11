a4fa6e970cb7159f1acfa6255a9c16d19afd5982
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 12:35:33 -0700

    edit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numb = 0;
+	char caracter = 'a';
+
+	do {
+	putchar (numb);
+	numb++;
+	} while (numb <= 9)
+
+	do {
+	putchar (caracter);
+	caracter++;
+	} while (caracter <= 'f');
+
+	putchar ('\n');
+
+return (0);
+
+}
