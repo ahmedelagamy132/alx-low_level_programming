d537bfc08b5c5f886016051bafd461b1ab8bcca0
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 11:59:42 -0700

    edit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	int  number = 0;
+
+	do {
+	printf(number);
+	number++;
+	} while (number <= '9');
+
+	printf('\n');
+
+return (0);
+}
