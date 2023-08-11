d597dbf55bc7b0562d3554c97bdb90ab910a61e2
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 14:38:51 -0700

    edit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numb = 0, i = 0, j = 0;
+
+	while (i <= 9)
+	{
+	while (j <= 9)
+	{
+	do {
+	if (i != numb && numb != j && j != i)
+	{
+	if (numb > i)
+	{
+	putchar (i + '0');
+	putchar (j + '0');
+	putchar (numb + '0');
+	if (i != 7)
+	{
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	numb++;
+	} while (numb <= 9);
+	j++;
+	numb = 0;
+	}
+	j = 0;
+	i++;
+	}
+
+	putchar('\n');
+return (0);
+}
