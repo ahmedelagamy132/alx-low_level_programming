cd398cf95c0ae2b6611f9899bf25ecb7f95ff59b
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 12:07:19 -0700

    edit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	char numb = '0';
+
+	do {
+	putchar (numb);
+	numb++;
+	} while (numb <= '9');
+
+	putchar('\n');
+
+return (0);
+}
