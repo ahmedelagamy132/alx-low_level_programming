1ff2a4c9956217a504952ee1eab6780a7c181fc9
ahmed <ahmedelagamy132@gmail.com>
2023-08-11 10:51:45 -0700

    edit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - hfghgfh
+ * Return: 0
+ */
+
+int main(void)
+{
+	char caracter = 'a';
+
+	do {
+	putchar (caracter);
+	} while (caracter <= 'z')
+
+
+
+
+
+
+
+return (0);
+}
