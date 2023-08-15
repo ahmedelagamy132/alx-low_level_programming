ebb42fc231d06feedf0b6fb62499e78c1a31254e
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 07:33:44 -0700

    edit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add-dljns
+ * @AAA: sdfdsfsdf
+ * @BBB:dfdsfsdfds
+ * Return: sum
+ */
+
+
+int add(int AAA, int BBB);
+{
+/*add two mnu*/
+int sum = AAA + BBB;
+return (sum);
+}
