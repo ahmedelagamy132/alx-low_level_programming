d6dbdca6b01a1817d37ad030aabae0dda414b120
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 16:29:26 -0700

    edit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+unsigned long first = 1, sec = 1, tot, number;
+
+
+printf("%d, %d, ", first ,sec);
+
+for (number = 3; number <= 98; number++) {
+tot = first + sec;
+first = sec;
+sec = tot;
+
+if (number % 31 == 0) {
+printf("\n");
+}
+printf("%lu, ", tot);
+}
+
+printf("\n");
+return 0;
+}
