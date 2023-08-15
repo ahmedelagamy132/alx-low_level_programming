07a141252b29e6b30cbc232df41cb1f40327d1db
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 14:27:57 -0700

    edit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * main - lkadksjadks
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int num = 0, sum = 0;
+while(num < 1024)
+{
+if (((num % 3) == 0) && ((num % 5) == 0))
+{
+sum += num;
+}
+
+num++;
+}
+printf("%d\n",sum);
+}
