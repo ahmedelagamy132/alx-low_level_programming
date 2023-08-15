a3c760a7f9b68199f8778228b09cac1bf40a752f
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 02:40:15 -0700

    edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,16 @@
 int main(void)
 {
 
-
-printf("_putchar\n");
-
+//I use this to print one char at once
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 
 return (0);
 }
