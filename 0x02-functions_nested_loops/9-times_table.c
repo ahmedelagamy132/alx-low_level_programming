b458802242dc11a55aa2ceb142152c6478d6bf37
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 08:44:24 -0700

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * times_table - mfg,dfgdfml
+ * times_table - hfghfghfghgh
  */
-
 
 
 
 void times_table(void)
 {
+
 
 int k = 0, j = 0;
 while (k < 10)
@@ -15,15 +15,35 @@ while (k < 10)
 while (j < 10)
 {
 /*print times table */
-int timees = k * j;
-_putchar(timees + '0');
+int check, timees = k * j;
+if ((timees / 10) != 0)
+{
+_putchar((timees / 10) + '0');
+_putchar((timees % 10) + '0');
+if (j != 9)
+{
 _putchar(',');
 _putchar(' ');
+}
+}
+else
+{
+_putchar((timees % 10) + '0');
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+
+
 j++;
 }
 j = 0;
 k++;
 _putchar('\n');
 }
-
 }
+
+
