3b4e2b4684c1fb6d84325bb9e944db7fbc0afbf3
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 06:49:13 -0700

    edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@ void jack_bauer(void)
 int k = 0, j = 0, hour1, hour2, min1, min2;
 while (k++ < 24)
 {
-while (l++ < 60)
+while (j++ < 60)
 {
 /*print the clock */
 hour1 = k % 10, hour2 = k / 10, min1 = j % 10, min2 = j / 10;
