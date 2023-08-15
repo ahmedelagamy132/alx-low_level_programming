cb77e90281c9321a5064e300303c8d912c8264f6
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 14:16:47 -0700

    edit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,44 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_times_table-rrthrtgtrghrt
- * 
- * @n: ;fsokflsg
+ *
+ *  print_times_table - lnasjcasuuascsajcs
+ *  
+ * @n:   dvks;ldkfsd
  */
 void print_times_table(int n)
 {
-int loop1 = 0, loop2, timeees, l;
+int loop1, timeees, loop2;
+/* listedddddddddddd loops */
 if (n <= 15 && n >= 0)
 {
-for (; loop1 <= n; loop1++)
-{   
-printf("0");
-loop2 = 1;
-for (; loop2 <= n; loop2++)
+for (loop1 = 0; loop1 <= n; loop1++)
 {
-timeees = loop2 * loop1;
-if((timeees / 100) != 0)
+_putchar(49 - 1);
+for (loop2 = 1; loop2 <= n; loop2++)
 {
-l =timeees / 10;
-_putchar(',');
-_putchar(' ');
-_putchar((timeees /100) + '0');
-_putchar((l%10) + '0');
-_putchar((timeees % 10) + '0');
-}
-else if((timeees / 10) != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((timeees / 10) + '0');
-_putchar((timeees % 10) + '0');
-}
-else
-{  
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar((timeees % 10) + '0');
-}
 
+_putchar(',');
+_putchar(' ');
+
+timeees = loop1 * loop2;
+
+if (timeees < 10)
+_putchar(' ');
+
+if (timeees < 100)
+_putchar(' ');
+
+if (timeees > 99)
+{
+_putchar((timeees / 100) + 48);
+_putchar(((timeees / 10)) % 10 + 48);
+}
+else if (timeees < 100 && prod > 9)
+{
+_putchar((timeees / 10) + 48);
+}
+_putchar((timeees % 10) + 48);
 }
 _putchar('\n');
 }
