e63a880c7acd823bb0ce41d1200424d741d52e1c
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 15:33:01 -0700

    edit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - bbbjjjjsfd
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+long numtobeadded1 = 1, numto_be_added2 = 2, added_mum, increasing = 2,sum = 0;
+added_mum = numtobeadded1 + numto_be_added2;
+
+
+
+printf("%ld, %ld, ", numtobeadded1, numto_be_added2);
+
+
+while (added_mum < 4000000)
+{
+
+
+increasing++;
+numtobeadded1 = numto_be_added2;
+numto_be_added2 = added_mum;
+added_mum = numtobeadded1 + numto_be_added2;
+}
+
+return (0);
+}
