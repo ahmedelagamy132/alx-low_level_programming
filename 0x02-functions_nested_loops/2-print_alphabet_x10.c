29e230e179ac78e2c5756f5ccf42c48c692bc4b8
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 05:16:55 -0700

    edit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
 void print_alphabet_x10(void)
 {
 int i = 0;
-while(i < 10 )
+while (i < 10)
 {
 char hh = 'a';
 while (hh <= 'z')
@@ -15,6 +15,7 @@ _putchar(hh);
 hh++;
 }
 _putchar('\n');
-}
 i++;
+}
+return;
 }
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - sdlkfmsdvmdsv
+ * @c : fgfdgf
+ * Return: 0 ,1
+ */
+int _isalpha(int c)
+{
+
+if (isalpha(c) > 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+
+
+
+}
