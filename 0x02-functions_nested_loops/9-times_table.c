d6270be3af39832a66a1e80d3567cdf140a235eb
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 07:26:01 -0700

    edit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - mfg,dfgdfmlllvlld
+ *
+ */
+
+void times_table(void)
+{
+int k = 0, j = 0, hour1, hour2, min1, min2;
+while (k < 10)
+{
+while (j < 10)
+{
+/*print times table */
+_putchar((k * j) + '0');
+
+}
+j = 0;
+k++;
+}
+}
