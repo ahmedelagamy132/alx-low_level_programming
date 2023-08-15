99365622231b0b4c60c9a448dd55f9e3b62f9d6e
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 15:07:02 -0700

    edit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,19 +9,19 @@
 int main(void)
 {
 
-long long num_to_be_added1 = 1, num_to_be_added2 = 2, added_mum;
+long num_to_be_added1 = 1, num_to_be_added2 = 2, added_mum;
 added_mum = num_to_be_added1 + num_to_be_added2;
 
 
 
-printf("%d, %d, ", num_to_be_added1, num_to_be_added2);
+printf("%lli, %lli, ", num_to_be_added1, num_to_be_added2);
 
 
 int increasing = 2;
-while (increasing < 50)
+while (increasing < 100)
 {
 
-if (increasing != 50)
+if (increasing != 100)
 {
 printf("%lli, ", added_mum);
 }
