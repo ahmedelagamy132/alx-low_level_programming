26ebca38f27e2891d80abe9a72547c9fb7e14827
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 10:36:35 -0700

    edit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+
+#include "main.h"
+/**
+ * print_to_98 - hgfmvsdbvdslnc
+ * @n: vd'fvdfvf
+ *
+ *
+ */
+
+
+void print_to_98(int n);
+{
+while (1)
+{
+if (n < 98)
+{
+int up = n++;
+_putchar(up + '0');
+if (up == 98)
+{
+break;
+}
+}
+
+else if (n > 98)
+{
+int down = n--;
+_putchar(down + '0');
+if (down == 98)
+{
+break;
+}
+}
+}
+}
+
+}
