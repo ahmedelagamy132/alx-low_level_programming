78ba5850e221f9f9f750511d59d0a89a341a9688
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 05:55:27 -0700

    edit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - lhjvkjhugkhbggggg
+ * @lastmun: mmmmn,vmclapooorer
+ * Return: lastmunhh
+ */
+
+int print_last_digit(int lastmun);
+{
+lastmun %= 10;
+return (lastmun);
+}
