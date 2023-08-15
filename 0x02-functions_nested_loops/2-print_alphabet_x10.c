e436c04c92a2d362fb255ad2c097accf9fa2a302
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 03:28:13 -0700

    edit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void 2-print_alphabet_x10.c(void)
+{
+int i = 0;
+while(i < 10 )
+{
+char hh = 'a';
+while (hh <= 'z')
+{
+_putchar(hh);
+hh++;
+}
+_putchar('\n');
+return;
+}
+i++;
+}
