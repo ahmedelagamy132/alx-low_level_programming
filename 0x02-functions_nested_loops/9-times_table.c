8fa8c0a4dfbf88ae43aa7d4aa8574a0d4611cebf
ahmed <ahmedelagamy132@gmail.com>
2023-08-15 10:18:45 -0700

    edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,37 +7,40 @@
 
 void times_table(void)
 {
-
-
 int k = 0, j = 0;
 while (k < 10)
 {
 while (j < 10)
 {
-/*print times table */
 int timees = k * j;
 if ((timees / 10) != 0)
 {
+_putchar(',');
+_putchar(' ');
 _putchar((timees / 10) + '0');
 _putchar((timees % 10) + '0');
-if (j != 9)
+}
+else if (timees != 0)
 {
 _putchar(',');
 _putchar(' ');
-}
+_putchar(' ');
+_putchar((timees % 10) + '0');
 }
 else
 {
-_putchar((timees % 10) + '0');
-if (j != 9)
+if (k == 0 && j != 9)
 {
+_putchar('0');
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 }
+else
+{
+_putchar('0');
 }
-
-
+}
 j++;
 }
 j = 0;
@@ -45,5 +48,3 @@ k++;
 _putchar('\n');
 }
 }
-
-
