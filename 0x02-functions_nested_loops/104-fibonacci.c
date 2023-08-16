66deb680ad24b584064d254c9ed896ef0ed6a865
ahmed <ahmedelagamy132@gmail.com>
2023-08-16 15:31:18 -0700

    edit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,9 +8,9 @@ int main(void)
 {
 unsigned long i, total, num2 = 2;
 unsigned long num1_1, num1_2, num2_1, num2_2, total1, total2, num1 = 1;
-printf("%ld, %ld, ", num1, num2);
-/*loop 9g times*/
-for (i = 0; i < 96; i++)
+printf("%ld, %ld, ",num1,num2);
+/*loops 96 times*/
+for(i = 0; i < 96; i++)
 {
 if (i < 90)
 {
@@ -22,18 +22,21 @@ else
 {
 if (i == 90)
 {
-num1_1 = num1 / 10000000000, num1_2 = num1 % 10000000000;
-num2_1 = num2 / 10000000000, num2_2 = num2 % 10000000000;
+num1_1 = num1 / 10000000000;
+num1_2 = num1 % 10000000000;
+num2_1 = num2 / 10000000000;
+num2_2 = num2 % 10000000000;
 }
 total1 = num1_1 + num2_1;
 total2 = num2_2 + num1_2;
-/*checks if the second part of number is very large*/
-if ((num2_2 + num1_2) > 99999999999)
+/*checks the sec part if it is very large)*/
+if ((num2_2 + num1_2)  > 9999999999)
 {
-total2 = total2 % 10000000000;
+    total2 = total2 % 10000000000;
+
 total1 = total1 + 1;
 }
-if (i == 95)
+if(i == 95)
 {
 printf("%lu%lu\n", total1, total2);
 }
