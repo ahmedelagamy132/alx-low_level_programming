8f52adb1ec26c42a8be0b0825fcf23291b989b43
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 15:18:32 -0700

    edit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,13 +12,13 @@ int num_p = n;
 if (num_p < 0)
 {
 num_p *= -1;
-putchar('-');
+_putchar('-');
 }
 /*if zero it will not print*/
 if (num_p / 10)
 {
 /*recursion to print before last1*/
-print(num_p / 10);
+print_number(num_p / 10);
 }
 /*print last*/
 _putchar(num_p % 10 + '0');
