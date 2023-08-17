e9a54e411582b321114466eb7e700e613581b0ff
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 16:44:57 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * print_square - xfvvvxfd
+ *
+ * @size: dfsmdfmskgf
+ */
+void print_square(int size)
+{
+int height = 0, width = 0;
+
+if (!(size > 0))
+{
+_putchar('\n');
+}
+else
+{
+/*for loop for the height*/
+for (; height < size; height++)
+{
+/*for loop for the width*/
+for (; width < size; width++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ void print_number(int n);
 int _putchar (char ss);
 void print_number(int n);
 void print_triangle(int size);
+void print_square(int size);
