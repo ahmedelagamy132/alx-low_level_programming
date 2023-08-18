24f8943394447aed9cea6ea9458e7c8a23e13b4f
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 17:52:33 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - cfhhgfxhvbxg
+ */
+
+
+void  print_numbers(void)
+{
+int nums;
+nums = 0;
+/*loop to print from 0 to 9*/
+for (; nums <= 9; nums++)
+{
+_putchar(nums + 48);
+}
+_putchar('\n');
+}
