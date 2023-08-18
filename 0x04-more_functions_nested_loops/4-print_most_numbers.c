aca8d4eb4e204a267eeab29d607defd59d250fbc
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 17:44:41 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - jfgfbccvbfddb
+ *
+ */
+void print_most_numbers(void)
+{
+int mostnum;
+
+for (mostnum = 0; mostnum <= 9; mostnum++)
+{
+if ((mostnum != 2) && (mostnum != 4))
+{
+_putchar('mostnum' + 48);
+}
+}
+_putchar('\n');
+}
