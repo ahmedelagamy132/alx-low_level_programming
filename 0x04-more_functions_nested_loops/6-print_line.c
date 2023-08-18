44447db11c0d6deba81e01a4384be5f3faf5e7b9
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 17:15:26 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - dsfjjnsdkcm
+ *
+ * @n: aslnakdmasds
+ */
+
+void print_line(int n)
+{
+
+int line;
+line = 0;
+/*loop for the line*/
+for (; line < n; line++)
+{
+/*95 for underscore*/
+_putchar(95);
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@ void print_number(int n);
 void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);
+void print_line(int n);
