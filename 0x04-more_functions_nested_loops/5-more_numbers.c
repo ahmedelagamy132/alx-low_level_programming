fbd197935af20add0ddd5ae72bca975cba54a7ed
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 17:35:49 -0700

    edit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,18 +15,14 @@ loop22 = 0;
 /*loop to print the 15 numbers*/
 for (; loop22 < 15; loop22++)
 {
-if (loop22 >= 10)
-{
 fir = loop22 / 10;
 sec = loop22 % 10;
-_putchar(fir + 48);
-_putchar(sec + 48);
-}
-else
+if (loop22 >= 10)
 {
+_putchar(fir + 48);
+}
 _putchar(loop22 + 48);
 
-}
 }
 _putchar('\n');
 }
