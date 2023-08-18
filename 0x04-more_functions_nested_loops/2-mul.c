a5431dc2aa592ceee86f9cadce1e6297be63e457
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 17:56:25 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - sdvkmc;lvmsdv
+ *
+ * @a: svdsdvdl,vdcx
+ *
+ * @b: blosgjlfkdm
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+/*return thee times*/
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,3 +8,4 @@ void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
 void print_numbers(void);
+int mul(int a, int b);
