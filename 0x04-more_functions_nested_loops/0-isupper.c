25d9454b3efb9a51c45847ccd00acdf3a33ee050
ahmed <ahmedelagamy132@gmail.com>
2023-08-17 18:06:57 -0700

    edit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - sdvldsnmlkdsmv
+ *
+ * @c: lokhokrthob
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+if (isupper(0) > 0)
+	return (1);
+else
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,4 @@ void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
+int _isupper(int c);
