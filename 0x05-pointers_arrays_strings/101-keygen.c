861715a8e0fdb64804eeb341f4976c776a5c2b11
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 12:46:50 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - dkv;sldfvsd
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int TotalSum, limit = 2645;
+char Printed_char;
+
+/*genaerate rand num*/
+srand(time(NULL));
+
+/*print paassworfd*/
+while (TotalSum <= limit)
+{
+TotalSum += (rand() % 128);
+putchar(rand() % 128);
+}
+/*last char*/
+putchar(2772 - TotalSum);
+return (0);
+}
