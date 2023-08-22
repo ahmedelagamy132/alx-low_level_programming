36e4480df34de266fae426e1569af2b4f2f86b41
ahmed <ahmedelagamy132@gmail.com>
2023-08-22 12:59:51 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - lknblkgbm;lcbv
+ *
+ * @dest: sdfjnsmd;lvmsovd
+ *
+ * @src: cskdjvnsfsvxc
+ * Return:kdlbmdlkgcm
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(src, dest);
+
+return (dest);
+}
