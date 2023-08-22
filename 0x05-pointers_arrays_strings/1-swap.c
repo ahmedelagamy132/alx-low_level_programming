06cf95bc2dcc867b95cfedabcf65e52833b7df2e
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 18:02:48 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - cg',lvf
+ *
+ * @a: fs;kmsf
+ *
+ * @b: sdfkmrlsdv
+**/
+
+void swap_int(int *a, int *b)
+{
+
+int one;
+
+one = *two;
+
+*two = *three;
+
+*three = one;
+}
