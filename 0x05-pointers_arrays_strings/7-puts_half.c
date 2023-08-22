5a1ea264c4b04d87f247696071feeedff307f7de
ahmed <ahmedelagamy132@gmail.com>
2023-08-22 12:35:18 -0700

    edit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,10 +13,12 @@ while (str[elementsnum] != '\0')
 {
 elementsnum++;
 }
+/*inc for the int*/
+
 elementsnum = elementsnum / 2;
-while (str[elementsnum] != '\0')
+while (str[elementsnum + 1] != '\0')
 {
-_putchar(str[elementsnum]);
+_putchar(str[elementsnum + 1]);
 elementsnum++;
 }
 _putchar('\n');
