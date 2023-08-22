0c819a1b2e6a2f8718f20c4096c8646b8e2095e3
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 18:42:33 -0700

    Edit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - yghljlkh
+ *
+ * @s: hbkln
+**/
+
+void print_rev(char *s)
+{
+int increment;
+/*gets the length os string*/
+for (increment = 0; s[increment] != 0; i++)
+	;
+/*print s in reverse*/
+while (increment--)
+{
+_putchar(s[increment]);
+}
+
+_putchar('\n');
+}
+
