5ece52789d32dcf59907556eeaca3b3bf9c9ccb0
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 19:22:25 -0700

    edit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
 /**
- * _atoi - dvl;smgkfmgDff
+ * _atoi - jkgnfb.kfmv
  *
- * @s: fsd;lg,fsg
- *Return: Tbfbfgbg
+ * @s: fdmnmdfv .df/v
+ *
+ *Return: dbmfmldmf/v
  */
 
 int _atoi(char *s)
 {
-int neg = 1, result;
-unsigned int toconcv = 0;
+/*+ve only*/
+unsigned int digits = 0;
+int answer, neg = 1;
 
 while (*s)
 {
+/*change t o-ve*/
 if (*s == 45)
-	neg -= 2;
-
+	neg = -neg;
+/*gets the num*/
 else if (*s <= 57 && *s >= 48)
 {
-toconcv = (toconcv * 10) + (*s - 48);
+digits = (digits * 10) + (*s - 48);
 }
-else if (toconcv > 0)
+/*break*/
+else if (digits > 0)
 {
 break;
 }
-(*s++);
+s++;
 }
-result = toconcv *neg;
-return (result);
+answer = digits *neg;
+	return (answer);
 }
