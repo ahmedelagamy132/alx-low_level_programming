dc03de851ea34f6d08845859e791505604dc0f15
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 18:23:40 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - jdlnvfknbkgldmbg
+ *
+ * @str: vfdjnvkfmvf
+**/
+
+void _puts(char *str)
+{
+int numm = 0;
+while( str[numm] = '\0')
+{
+_putchar(str[numm]);
+i++;
+}
+_putchar('\n');
+}
