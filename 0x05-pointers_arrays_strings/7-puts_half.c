3530171c85b1b36c6a8db9d66b195ade59db5a7b
ahmed <ahmedelagamy132@gmail.com>
2023-08-22 12:28:09 -0700

    edit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,14 +2,21 @@
 
 /**
  * puts_half - lknblkgbm;lcbv
+ *
  * @str: lmbklcmvmkg
  */
 
-void puts_half(cha;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	if (len % 2 == 0)
-	{
-		for (n = (len / 2); n < len; n++)
-			_putchar(
+void puts_half(char *str)
+{
+int elementsnum = 0;
+while (str[elementsnum] != '\0')
+{
+elementsnum++;
+}
+elementsnum = elementsnum / 2;
+while (str[elementsnum] != '\0')
+{
+putchar(str[elementsnum]);
+elementsnum++;
+}
+}
