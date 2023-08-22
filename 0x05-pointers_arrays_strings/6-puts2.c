d1a50de3c9f9b56b703170e4a4d11a700d325120
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 19:31:32 -0700

    edit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * puts2 - sdkmv.,s
+ *
+ * @str: vds.vmlkvscf
+ */
+
+void puts2(char *str)
+{
+int howtall = 0;
+
+while (str[howtall] != '\0')
+{
+/*prints only the -ve*/
+if (len % 2 == 0)
+_putchar(str[howtall]);
+howtall++;
+}
+_putchar('\n');
+}
