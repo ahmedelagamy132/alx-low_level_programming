f5d83db3717c0b1a08a4413ff5c671fe35927f5e
ahmed <ahmedelagamy132@gmail.com>
2023-08-21 19:02:22 -0700

    Edit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - ;okd,;lgfd
+ * @s: dklskmflkdf
+**/
+
+void rev_string(char *s)
+{
+char help;
+int invr, strup = 0,tqll = 0;
+
+while (s[tqll++] != '\0';)
+{
+tqll++;
+}
+/*swapping*/   
+invr = len - 1;
+while (invr >= tqll / 2)
+{
+help = s[invr];
+
+s[invr] = s[strup];
+
+s[strup] = help;
+
+strup++;
+}
+}
