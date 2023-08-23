1a730508ee5a022f4b8ccf2aab38222409b37674
ahmed <ahmedelagamy132@gmail.com>
2023-08-23 14:58:15 -0700

    edit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 
-int longg = 0, longg2 = 0;
+int longg = 0, longg2 = 0, iter;
 
 while (src[longg2] && longg2 < n)
 {
 dest[longg] = src[longg2];
 longg++;
 longg2++;
+}
+for (i = 0; i < n; i++)
+{
+dest[iter] = '\0';
+iter = iter + 1;
 }
 return (dest);
 
