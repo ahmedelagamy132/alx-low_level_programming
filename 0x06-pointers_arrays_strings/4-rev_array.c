252e516159c46af3f0ccaae87546f642706f80b3
ahmed <ahmedelagamy132@gmail.com>
2023-08-25 09:33:09 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - jhjgdlkgjdg
+ * @a: fdgdfgdfgdfg
+ * @n: ljdgndfjgndfgjndf
+ */
+
+void reverse_array(int *a, int n)
+{
+int loop = 0, temp, loop2 = n - 1;
+
+while (loop < loop2)
+{
+temp = a[loop];
+a[loop] = a[loop2];
+a[loop2] = temp;
+loop++;
+loop2--;
+}
+}
