0b8f1835c257455fc9cde31a75fba803f1198e42
ahmed <ahmedelagamy132@gmail.com>
2023-08-25 09:45:25 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - cljfnvdfkmv
+ *
+ * @n: dvkjsdvsd
+ *
+ * Return: bsacbjksd
+ */
+char *string_toupper(char *n)
+{
+int loop = 0;
+
+while (n[loop] != '\0')
+{
+
+toupper(n[loop]);
+
+
+}
+
+return (n);
+}
