f63a1db3e20336ad59a766e5b9e8d420e82fc2fe
ahmed <ahmedelagamy132@gmail.com>
2023-08-25 08:50:56 -0700

    Edit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,13 @@ int _strcmp(char *s1, char *s2)
 {
 int loop = 0, difference;
 
-for (loop; s1[loop] != '\0' && s2[loop] != '\0'; loop++)
+for (; s1[loop] != '\0' && s2[loop] != '\0'; loop++)
 {
 if (s1[loop] != s2[loop])
 {
-difference = s1[loop] - s2[loop]
+difference = s1[loop] - s2[loop];
 break;
 }
-return (difference);
 }
+return (difference);
 }
