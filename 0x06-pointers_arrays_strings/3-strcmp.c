acec00aeef528cbbb5d3d21535fd5e6e25655017
ahmed <ahmedelagamy132@gmail.com>
2023-08-25 08:37:48 -0700

    edit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - fhdkndfjngdfgfd
+ * @s1: dlfjgflkdjg
+ * @s2: ndfjvndjfvndfvfd
+ * Return: jdnljdnvljdnmfv
+ */
+
+
+
+int _strcmp(char *s1, char *s2)
+{
+int loop = 0, difference;
+
+for (loop; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+if (s1[loop] != s2[loop])
+{
+difference = s1[loop] - s2[loop]
+break;
+}
+return (difference);
+}
+}
