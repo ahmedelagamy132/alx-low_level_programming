6c2d88248bcfe667c8fe72c70526af7698281521
ahmed <ahmedelagamy132@gmail.com>
2023-08-28 12:21:24 -0700

    edit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_chessboard - ajdchnldkvmcx
+ *
+ * @a: x cljvnkxcmxc
+ *
+ * Return: xckvj,nxkvxkmc
+ */
+void print_chessboard(char (*a)[8])
+{
+int loop1 = 0 ,loop2 = 0;
+
+while (loop1 < 8)
+{
+/*getting the location*/
+while (loop2 < 8)
+{
+_putchar(a[loop1][loop2]);
+
+_putchar('\n');
+
+loop2++;
+}
+loop2 = 0;
+loop1++;
+}
+}
+
