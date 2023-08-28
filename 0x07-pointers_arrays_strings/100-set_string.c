1b5b9efe618028389af07f581556f3cec37f7d87
ahmed <ahmedelagamy132@gmail.com>
2023-08-28 12:59:53 -0700

    edit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_string - jdbdvnf,vcx
+ *
+ * @s: vnsjvnf,sjncm
+ *
+ * @to: khnkfvcx, ,
+ */
+
+void set_string(char **s, char *to)
+{
+/*setting string*/
+
+*s = to;
+
+
+}
+
