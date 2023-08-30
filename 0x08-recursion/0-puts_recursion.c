03f21b46e5c1b82c354ff5799bac52563f81d679
ahmed <ahmedelagamy132@gmail.com>
2023-08-29 22:24:29 -0700

    edit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - hfvbnkdfjvnkfvnfcbcbjc
+ * s : csmbvkhfvbkxjdfv
+ */
+void _puts_recursion(char *s)
+{
+int looop = 0;
+while (s[looop] != '\0')
+{
+printf("%c", s[looop]);
+
+
+}
+printf("\n");
+
+
+
+}
