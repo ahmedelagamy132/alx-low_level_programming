5b6eaf29845aae9dc585701218cc7c15a1a4cfc2
ahmed <ahmedelagamy132@gmail.com>
2023-08-29 23:45:58 -0700

    edit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *factorial - fbvnkcbml/mdcf,b/l,cv l/bmcv
+ *@n: jv,cbnlfkbn.lknvmcv
+ *Return: df,jdfk.jmbl/,cvl/m bc
+ */
+int factorial(int n)
+{
+/*condition to get the factorial*/
+if (n == 0)
+{
+
+	return (1);
+}
+else if (0 > n)
+{
+return (-1);
+}
+else
+{
+
+return (factorial(n - 1) * n);
+
+}
+
+}
