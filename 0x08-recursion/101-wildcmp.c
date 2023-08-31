b36d5a18efcc15820681df565ac81f9b87b2bbf8
ahmed <ahmedelagamy132@gmail.com>
2023-08-31 07:33:10 -0700

    edit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,40 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - jncds,jvnfcx
+ * wildcmp - df,gjndf,gnfdjgdf
+ *
+ * @s1: fdgldfmkgmdfkgdf
+ *
+ * @s2: fdghn,jgdfnkjgmdfg
+ *
+ * Return: .kmvldfgnjdfg,m,fd
  */
-void main(void)
+
+
+int wildcmp(char *s1, char *s2)
 {
-printf("");
+/*listed if to check*/
+if (*s2 == '*')
+{
+if (*s1 == '\0' && *(s2 + 1) != '\0')
+{
+return (0);
+}
+else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+{
+return (1);
+}
+}
+else if(*s2 == '\0')
+{
+if (*s1 == '\0')
+{
+return (1);
+}
+}
+else if (*s2 == *s1)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
 }
