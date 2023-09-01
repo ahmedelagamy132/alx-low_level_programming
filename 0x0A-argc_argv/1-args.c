9b4c114ae4c90de59f154425f1713fe439f4d41a
ahmed <ahmedelagamy132@gmail.com>
2023-09-01 10:43:00 -0700

    edit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - f,vjnf,jvfc
+ * @argc: dfgdfg
+ * @argv: dfgdfgdf
+ * Return: fdgdfgd
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
