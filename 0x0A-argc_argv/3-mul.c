0ed9fc6bed9a763da8704d937f2e2d833d1e2e70
ahmed <ahmedelagamy132@gmail.com>
2023-09-01 10:21:36 -0700

    edit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - mdfhbvn,dfnv dfv
+ * @argc: vdjfvn,jdfnb dfv 
+ * @argv: df,jvn ,dfmnvkdfmnv
+ * Return: dfvjndfvndjvn
+ */
+ 
+int main(int argc, char *argv[])
+{
+int num1 = 0, num2 = 0;
+
+if (argc < 2)
+{
+printf("Error\n");
+}
+else
+{
+num2 = atoi(argv[2]), num1 = atoi(argv[1]);
+printf("%d\n", num1 * num2);
+}
+return (0);
+}
