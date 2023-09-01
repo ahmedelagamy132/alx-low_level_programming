d416f1dbc9c119e13a9b1f12fc628787e207e1ff
ahmed <ahmedelagamy132@gmail.com>
2023-09-01 09:51:23 -0700

    edit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * main -add positive numbers
- * @argc: int
- * @argv: char *
- * Return: always 0
+ * main -dsjfhsdjfd
+ * @argc: sdfsdfsdf
+ * @argv: sdfdsfdsf
+ * Return: sdfdsfsdfsdf
  */
 
 int main(int argc, char *argv[])
