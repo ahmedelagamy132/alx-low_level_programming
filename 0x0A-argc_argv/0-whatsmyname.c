dd7aeb2f358bce5fda2a9cb11ad5dda15c987e5c
ahmed <ahmedelagamy132@gmail.com>
2023-09-01 03:29:41 -0700

    edit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - fjldvndjbngvjcb
+ *
+ * @argc: dsvjsfvnf,jvnfv
+ *
+ * @argv: ,mnfvj,fxnvvvvv
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
