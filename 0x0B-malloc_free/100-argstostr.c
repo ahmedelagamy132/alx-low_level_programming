eed9b0686b195ce9987eaaa8ebe6f6a87ae0685d
ahmed <ahmedelagamy132@gmail.com>
2023-09-05 04:08:46 -0700

    edit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - afbdsjnvxknvcv
+ *
+ *@ac: fvdj,hvkdfjvc
+ *
+ *@av: xcvjhdfjvcm
+ *
+ * Return: djbvn ,jcv n,cjvb c
+ */
+char *argstostr(int ac, char **av)
+{
+char *arr1;
+int loop1 = 0, loop2 = 0, loop3 = 0, length = 0, mal;
+arr1 = null;
+if (ac == 0)
+	return (NULL);
+if (av == NULL)
+	return (NULL);
+while (loop1 < ac)
+{
+while (av[loop1][loop2] != '\0')
+{
+length++;
+loop2++;
+}
+loop2 = 0;
+loop1++;
+}
+mal = length + ac + 1;
+arr1 = malloc(sizeof(char) * mal);
+if (arr1 == NULL)
+{
+return (NULL);
+}
+loop1 = 0;
+loop2 = 0;
+while (loop1 < ac)
+{
+while (av[loop1][loop2] != '\0')
+{
+arr1[loop3] = av[loop1][loop2];
+loop3++;
+loop2++;
+}
+arr1[loop3] = '\n';
+loop3++;
+loop1++;
+loop2 = 0;
+}
+arr1[loop3] = '\0';
+return (arr1);
+}
