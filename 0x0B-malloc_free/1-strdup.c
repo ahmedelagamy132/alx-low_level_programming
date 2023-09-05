65dc54912d22e42b0fc2a28aca0980ce5012a990
ahmed <ahmedelagamy132@gmail.com>
2023-09-05 01:07:12 -0700

    edit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strdup - dfjb ndlkbdkflbvg
+ * @str: dvjnbkgdmkbg,vcv
+ * Return: dgljbndkbnjg
+ */
+char *_strdup(char *str)
+{
+int loop, amount, mall;
+char *arr1;
+
+if (str == NULL)
+{
+return (NULL);
+}
+amount = 0;
+while (str[amount] != '\0')
+{
+amount++;
+}
+mall = amount *sizeof(*str);
+arr1 = malloc(mall + 1)
+
+if (arr1 != 0)
+{
+while (loop < amount)
+{
+arr1[loop] = str[loop];
+loop++;
+}
+}
+else
+{
+return (NULL);
+}
+return (arr1);
+}
