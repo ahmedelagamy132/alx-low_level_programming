f0c55ceb840b2f0ec71990a1704626b373a8c0d9
ahmed <ahmedelagamy132@gmail.com>
2023-09-05 02:38:05 -0700

    Edit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * **alloc_grid - vkhfbv j,fxnvjfv
+ *
+ * @width: vjfkvnjknvjcxv
+ *
+ * @height: vfjnvjfcnvdc
+ *
+ * Return: vfnbjfnvxmvc
+ */
+int **alloc_grid(int width, int height)
+{
+int loop1 = 0, loop2 = 0, **arr1;
+
+if (height <= 0)
+{
+return  (NULL);
+}
+if (width <= 0)
+{
+return (NULL);
+}
+arr1 = malloc(sizeof(int *) * height);
+if (arr1 == NULL)
+{
+return (NULL);
+}
+else
+{
+while (loop1 < height)
+{
+ptr[loop1] = malloc(sizeof(int) * width);
+if (ptr[x] == NULL)
+{
+while (loop2 < loop1)
+{
+free(ptr[loop2]);
+loop2++;
+}
+free(arr1);
+return (NULL);
+}
+while (loop2 < width)
+{
+arr1[loop1][loop2] = 0;
+loop2++;
+}
+loop1++;
+}
+return (arr1);
+}
+}
