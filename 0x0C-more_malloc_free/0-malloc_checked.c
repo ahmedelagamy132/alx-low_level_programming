3894766a6ce4240e7f9d29c90f2b0d325a437436
ahmed <ahmedelagamy132@gmail.com>
2023-09-07 04:58:52 -0700

    edit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - ajvnsf,mvkdfv
+ *
+ * @b: vs,fnvjdfmv
+ *
+ * Return: vdf.knvmdkf.vmdfkvmdffffff
+ */
+void *malloc_checked(unsigned int b)
+{
+int *pointer;
+pointer = malloc(b);
+if (pointer == NULL)
+{
+exit(98);
+}
+return (pointer);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+dgvnfd,ifdkvbndfj,
