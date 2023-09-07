ba78418ecb37a1c988fc17f9e3703b2955514d68
ahmed <ahmedelagamy132@gmail.com>
2023-09-07 06:30:32 -0700

    edo

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* array_range - dfnv d,n .kdv mc
+*
+* @min: cvjl n,cvnm kcv
+* @max: cvknc.kvccvi
+*
+* Return: cv k,cn lxkvn xv xc,mv c
+*/
+
+int *array_range(int min, int max)
+{
+int *word, loop1 = 0, loop2 = 0;
+
+if (max < min)
+{
+return (NULL);
+}
+word = malloc(sizeof(*word) * (1 + (max - min)));
+
+if (block != NULL)
+{
+loop1 = min;
+while (loop1 <= max)
+{
+block[loop2] = loop1;
+loop2++;
+loop1++;
+}
+return (block);
+}
+else
+{
+return (NULL);
+}
+}
