5208c51f4b98114ec8b94388c19b2bfeac31fbde
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-09-26 15:29:18 -0700

    edit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,24 +6,26 @@
  */
 char *leet(char *n)
 {
-char str1[] = "TeLtOAEloa";
-	char str2[] = "7317043104";
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+
+
 	int loop1 = 0, loop2 = 0;
 
 
 
-	while (n[loop1] != '\0')
-	{
-		while (loop2 < 10)
-		{
-			if (str1[loop2] == n[loop1])
-			{
-				n[loop1] = str2[loop2];
-			}
+while (n[loop1] != '\0')
+{
+while (loop2 < 10)
+{
+if (str1[loop2] == n[loop1])
+{
+n[loop1] = str2[loop2];
+}
 loop2++;
-		}
+}
+loop2 = 0;
 loop1++;
 }
 return (n);
 }
-
