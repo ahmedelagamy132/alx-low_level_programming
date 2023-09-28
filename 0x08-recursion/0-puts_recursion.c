6130b270e6f9f0ef55f3888c856b23f3e53abb40
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-09-28 01:13:45 -0700

    edit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - prints string
+ * @s:khfgjkfdnjgn
+ */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{	
+	{
 		_putchar('\n');
 		return;
 	}
