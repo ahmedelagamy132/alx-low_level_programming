b4597ef37848cb637543e2e37aef26e9d3754bac
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-02 12:49:17 -0700

    edit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - lfnslkdfmskdfmvsddc
+ *
+ * @c: lkmgf;slx,vsx,fv
+ *
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+if (isdigit(c) > 0)
+	return (1);
+else
+	return (0);
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _memcpy - fbvkjfnvkjnfkjbdvkfc
+ *
+ *@dest: v,ndnfvjnfdlvndfv
+ *
+ *@src: jkfnkcxvnfdkjc
+ *
+ *@n: skvjnf,jcnx
+ *
+ * Return:  mxcb vjnd
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+unsigned int loop = 0;
+
+/**copyong**/
+for (; loop < n; loop++)
+{
+
+dest[loop] = src[loop];
+
+
+}
+
+return (dest);
+}
