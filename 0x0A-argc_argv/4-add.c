8b0186683ce8b2553a7aa9289283aaa5a3ff4ea1
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-02 02:48:07 -0700

    edit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -  name
+ *@argc: argc
+ *@argv: argv
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int loop1 = 0, loop2 = 0, total = 0;
+
+if (argc < 2)
+{
+printf("0\n");
+}
+else
+{
+loop1 = 1;
+while (loop1 < argc)
+{
+for (loop2 = 0; argv[loop1][loop2] != '\0'; loop2++)
+{
+if (argv[loop1][loop2] > '9' || argv[loop1][loop2] < '0')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[loop1]);
+loop1++;
+}
+printf("%d", sum);
+printf("\n");
+}
+return (0);
+}
