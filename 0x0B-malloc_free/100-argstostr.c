f8179edb5e27553dd1ad25e65ab8f95a8d185c02
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-03 09:31:31 -0700

    edit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * *argstostr - arguments concatenated
+ * @ac: count
+ * @av: pointer
+ * Return: pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+char *word;
+int loop1 = 0, loop2 = 0, num = 0, cap = 0;
+
+if (av == 0)
+{
+return (NULL);
+}
+if (ac == NULL)
+{
+return (NULL);
+}
+
+while (loop1 < ac)
+{
+while (av[loop1][loop2] != '\0')
+{
+loop2++
+}
+loop2 = 0;
+cap += loop2 + 1; 
+loop1++;
+}
+word = malloc(cap + 1);
+if (word == 0)
+{
+return (NULL);
+}
+num = 0;
+loop1 = 0;
+loop2 = 0;
+while (loop1 < ac)
+{
+while (av[loop1][loop2] != '\0')
+{
+word[num] = av[loop1][loop2];
+num++;
+loop2++;
+}
+loop2 = 0;
+word[num] = '\n';
+num++;
+loop1++;
+}
+return (word);
+}
