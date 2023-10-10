2d42c4e6b5ff4448de8bf23683bdd10b6fbf3f9b
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-10 10:11:20 -0700

    edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,22 @@
-#ifndef ahmed
-#define ahmed
-
+#ifndef A
+#define A
 /**
- * struct dog - struct
+ * struct dog - dd
  * @name: name
  * @age: age
  * @owner: owner
  */
-struct dog
+
+typedef struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-};
-typedef struct dog dog_t;
+char *name;
+float age;
+char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 
 
 #endif
