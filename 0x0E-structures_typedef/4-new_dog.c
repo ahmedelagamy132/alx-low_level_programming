627b7798d3ab4ab8401b5d42978d533ba5a6e936
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-10 10:02:29 -0700

    edit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - dog
+ * @name: name
+ * @age: age
+ **
+ * @owner: owner
+ * Return: ptr
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *ptr;
+int loop1 = 0, loop2 = 0, loop3 = 0;
+	
+
+ptr = malloc(sizeof(dog_t));
+
+if (ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+else
+{
+while (name[loop1])
+{		
+loop1++;
+}
+while (owner[loop2])
+{
+loop2++;
+}
+ptr->name = malloc(++loop1);
+ptr->owner = malloc(++loop2);
+
+if (ptr->name == NULL || ptr->owner == NULL)
+{
+free(ptr->name);
+free(ptr->owner);
+free(ptr);
+return (NULL);
+}
+while (loop3 < loop1)
+{
+ptr->name[loop3] = name[loop3];
+loop3++;	
+}
+ptr->name[loop3] = '\0';
+loop3 = 0;
+while (loop3 < loop2)
+{
+ptr->owner[loop3] = owner[loop3];
+loop3++;	
+}
+ptr->owner[loop3] = '\0';
+ptr->age = age;
+return (ptr);
+}
+}
