3d9cd871d396e4b3b9ae340dd627710d55df4438
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-10 10:07:39 -0700

    edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,10 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *ptr;
 int loop1 = 0, loop2 = 0, loop3 = 0;
-	
-
 ptr = malloc(sizeof(dog_t));
-
 if (ptr == NULL)
 {
 free(ptr);
@@ -27,34 +24,30 @@ return (NULL);
 else
 {
 while (name[loop1])
-{		
-loop1++;
-}
+	loop1++;
 while (owner[loop2])
 {
 loop2++;
 }
 ptr->name = malloc(++loop1);
 ptr->owner = malloc(++loop2);
-
 if (ptr->name == NULL || ptr->owner == NULL)
 {
 free(ptr->name);
-free(ptr->owner);
-free(ptr);
+free(ptr->owner), free(ptr);
 return (NULL);
 }
 while (loop3 < loop1)
 {
 ptr->name[loop3] = name[loop3];
-loop3++;	
+loop3++;
 }
 ptr->name[loop3] = '\0';
 loop3 = 0;
 while (loop3 < loop2)
 {
 ptr->owner[loop3] = owner[loop3];
-loop3++;	
+loop3++;
 }
 ptr->owner[loop3] = '\0';
 ptr->age = age;
