4260d84c7aec40679466b72b2478993bf46ca07c
Ahmed ElAgamy <ahmedelagamy132@gmail.com>
2023-10-10 10:27:09 -0700

    edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,56 +1,54 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 
 /**
- * new_dog - dog
- * @name: name
+ * new_dog - new dog
+ * @name: dog
  * @age: age
- **
  * @owner: owner
- * Return: ptr
+ * Return: pointer
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *ptr;
 int loop1 = 0, loop2 = 0, loop3 = 0;
+
+if (name == NULL || owner == NULL)
+	return (NULL);
+	
 ptr = malloc(sizeof(dog_t));
 if (ptr == NULL)
+	return (NULL);
+	
+while (name[loop1])
+	loop1++;
+loop1++;
+ptr->name = malloc(loop1 * 1);
+if (ptr->name == NULL)
 {
 free(ptr);
 return (NULL);
 }
-else
-{
-while (name[loop1])
-	loop1++;
+for (loop3 = 0; loop3 < loop1; loop3++)
+	ptr->name[loop3] = name[loop3];
+ptr->age = age;
+loop2 = 0;
 while (owner[loop2])
-{
+	loop2++;
 loop2++;
-}
-ptr->name = malloc(++loop1);
-ptr->owner = malloc(++loop2);
-if (ptr->name == NULL || ptr->owner == NULL)
+ptr->owner = malloc(loop2 * 1);
+if (ptr->owner == NULL)
 {
+free(ptr);
 free(ptr->name);
-free(ptr->owner), free(ptr);
 return (NULL);
 }
-while (loop3 < loop1)
-{
-ptr->name[loop3] = name[loop3];
-loop3++;
-}
-ptr->name[loop3] = '\0';
 loop3 = 0;
 while (loop3 < loop2)
 {
 ptr->owner[loop3] = owner[loop3];
 loop3++;
 }
-ptr->owner[loop3] = '\0';
-ptr->age = age;
 return (ptr);
-}
 }
