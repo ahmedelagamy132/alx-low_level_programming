896d9acd0c4e0d4f369e1b26195acf8fe38d03f9
ahmedelagamy132 <ahmedelagamy132@gmail.com.com>
2023-10-19 04:40:40 -0700

    Edit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints list
+ * @h: ptr
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
