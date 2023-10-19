f81839cdba26dacf9c9cb0fb51c9378cfcb1eb91
ahmedelagamy132 <ahmedelagamy132@gmail.com.com>
2023-10-19 08:26:35 -0700

    edit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef Ahmed
+#define Ahmed
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,9 +11,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+#endif
