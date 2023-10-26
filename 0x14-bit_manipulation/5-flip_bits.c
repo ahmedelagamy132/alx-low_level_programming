30cb7aaafad39dd141f651449391936911debb69
ahmedelagamy132 <ahmedelagamy132@gmail.com.com>
2023-10-26 11:08:52 -0700

    edit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 unsigned int loop;
 
-for (nbits = 0; m || n; n >>= 1, m >>= 1)
+for (loop = 0; m || n; n >>= 1, m >>= 1)
 {
 
 if ((n & 1) != (m & 1))
